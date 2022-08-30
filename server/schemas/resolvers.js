9bd3f9c7ed80cfc7eab0656b8f84f67402d84b17
Shrike6 <cody.stubbs@gmail.com>
2022-08-29 19:39:31 -0500

    finished resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -2,19 +2,62 @@ const { AuthenticationError } = require('apollo-server-express');
 const { User } = require('../models');
 
 const resolvers = {
-    Query: {
-        me: async (parent, args, context) => {
-          if (context.user) {
-            const userData = await User.findOne({ _id: context.user._id }).select('--password');
-    
-            return userData;
-          }
-    
-          throw new AuthenticationError('Not logged in');
-        },
+  Query: {
+      me: async (parent, args, context) => {
+        if (context.user) {
+          const userData = await User.findOne({ _id: context.user._id }).select('--password');
+  
+          return userData;
+        }
+  
+        throw new AuthenticationError('Not logged in');
       },
+    },
 
-    Mutation: {
-        login: 
-    }
-}
+  Mutation: {
+    // login
+      login: async (parent, { email, password }, context) => {
+        const user = await User.findOne({ email });
+        if (!user) {
+            throw new AuthenticationError("Incorrect username.");
+        }
+
+        const checkPW = await user.isCorrectPassword(password);
+        
+        if (!checkPW) {
+            throw new AuthenticationError("Incorrect password.");
+        }
+
+        const token = signToken(user);
+
+        return {token, user};
+    },
+    // Adds a new user
+    addUser: async (parent, args) => {
+      const addUser = await User.create(args);
+      const token = signToken(user);
+
+      return { token, user };
+    },
+    saveBook: async (parent, { data }, context) => {
+      if (context.user) {
+        const updatedUser = await User.findOneAndUpdate(
+          { _id: user._id },
+          { $push: { savedBooks: body } },
+          { new: true }
+        );
+        return updatedUser;
+      };
+    },
+    deleteBook: async (parent, { bookId }, context) => {
+      const updatedUser = await User.findOneAndUpdate(
+        { _id: user._id },
+        { $pull: { savedBooks: { bookId } } },
+        { new: true }
+      );
+      return updatedUser;
+    },
+  };
+};
+
+module.exports = resolvers;
